7320683e442d42b710975f5a4eb777c880a0c80a
05011440214 <05011440214@JIPA-LABIN03-17.ifro.local>
2018-03-07 14:58:53 -0400

    Comit

---
+++ ProjetoGitJava/src/br/edu/ifro/Aula.java
@@ -0,0 +1,13 @@
+
+package br.edu.ifro;
+
+
+
+public class Aula {
+//criar metodo soma, subtração, divisão
+    
+    public static void main(String[] args) {
+      System.out.println("Hello Word");
+    }
+    
+}
---
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -0,0 +1,21 @@
+
+package br.edu.ifro;
+
+
+public class Calculadora {
+    
+    
+    public double Multipli(double n1,double n2, double n3){
+        
+        return (n1*n2)*n3;
+    }
+    
+    public double Subtrai(double n1,double n2, double n3){
+        
+        return (n1-n2)-n3;
+    }
+    public double Divisao(double n1,double n2, double n3){
+        
+        return (n1/n2)/n3;
+    }
+}
