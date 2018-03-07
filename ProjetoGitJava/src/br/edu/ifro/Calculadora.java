23351906299a990e66906e67212b635ccefb25f3
03590320281 <03590320281@JIPA-LABIN03-05.ifro.local>
2018-03-07 15:13:12 -0400

    calculadora aula ANDAMENTO

--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -9,14 +9,20 @@ public class Calculadora {
     return num1 - num2;    
     }
     public double Divisao(double num1, double num2){
-    return num1 * num2;    
+    return num1 / num2;    
     }
-    public double multiplicação(double num1, double num2){
-    return num1 / num2;   
+    public double Multiplicacao(double num1, double num2){
+    return num1 * num2;   
 } 
 
 public static void main (String [] Args){
-    
-}
+    double num1 = 10;
+    double num2 = 20;
+    Calculadora calculadora = new Calculadora ();
+    System.out.println (calculadora.Soma(num1,num2));
+    System.out.println (calculadora.Subtracao(num1,num2));
+    System.out.println (calculadora.Divisao(num1,num2));
+    System.out.println (calculadora.Multiplicacao(num1,num2));
 
+}
 }
