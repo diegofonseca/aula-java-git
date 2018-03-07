d5ca2da96d6ecc272f7fc25d5a3507193f974993
03590320281 <03590320281@JIPA-LABIN03-06.ifro.local>
2018-03-07 15:07:18 -0400

    Calculadora

--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -8,9 +8,7 @@ import sun.applet.Main;
  * @author 03358365263
  */
 public class Calculadora {
-    double n1,
-           n2;
-    
+
     public double Soma(double n1, double n2){
      return n1+n2;
 }
@@ -24,7 +22,12 @@ public class Calculadora {
 public static void main (String [] Args){
  double n1 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog("Digite o 1º número:"));
  double n2 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog("Digite o 2º número:"));
-
+ //double n1 = 10;
+ //double n2 = 20;
+ Calculadora calc = new Calculadora();
+    System.out.println(calc.Soma(n1,n2));
+    System.out.println(calc.Subtracao(n1,n2));
+    System.out.println(calc.Multiplicacao(n1,n2));
 }
 
 }
