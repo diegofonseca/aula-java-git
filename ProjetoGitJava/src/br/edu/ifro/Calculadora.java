daddd0fc80d2630f963ecbd0e3f911c365ce2da6
03590320281 <03590320281@JIPA-LABIN03-05.ifro.local>
2018-03-07 14:56:35 -0400

    calculadora aula teste

---
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -0,0 +1,22 @@
+package br.edu.ifro;
+
+public class Calculadora {
+
+    public double Soma(double num1, double num2){
+    return num1 + num2;   
+    }
+    public double Subtracao(double num1, double num2){
+    return num1 - num2;    
+    }
+    public double Divisao(double num1, double num2){
+    return num1 * num2;    
+    }
+    public double multiplicação(double num1, double num2){
+    return num1 / num2;   
+} 
+
+public static void main (String [] Args){
+    
+}
+
+}
