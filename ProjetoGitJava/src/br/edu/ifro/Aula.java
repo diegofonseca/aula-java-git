30e21d9ea4cc62ad09ee02a08bcd7be825503ff7
04607476226 <04607476226@JIPA-LABIN03-04.ifro.local>
2018-03-07 14:40:54 -0400

    Hello World

--- ProjetoGitJava/src/br/edu/ifro/Aula.java
+++ ProjetoGitJava/src/br/edu/ifro/Aula.java
@@ -15,7 +15,8 @@ public class Aula {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hello World");
     }
     
 }
