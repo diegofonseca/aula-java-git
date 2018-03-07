bb957cf005544d2289ccd3f671c0456247ba38cc
04592828240 <04592828240@JIPA-LABIN03-27.ifro.local>
2018-03-07 14:55:49 -0400

    Criando a classe calculadora

---
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+/**
+ *
+ * @author 04592828240
+ */
+public class Calculadora {
+
+    public double soma(double x, double y) {
+        return x + y;
+    }
+
+    public double subtracao(double x, double y) {
+        return x - y;
+    }
+
+    public double divisao(double x, double y) {
+        return x / y;
+    }
+
+    public double multiplicação(double x, double y) {
+        return x * y;
+    }
+}
