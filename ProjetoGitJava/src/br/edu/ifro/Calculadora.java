2cb90cb4091088cbe18d7f01c6288c293430161e
Amanda Reis <Amanda Reis@10.50.71.122>
2018-03-07 14:11:36 -0500

    Calculadora

--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -1,4 +1,6 @@
 
+
+
 package br.edu.ifro;
 
 
@@ -17,12 +19,18 @@ public double divisao (double  num1, double num2){
     return num1 / num2;
     
 }  
-public double Mutiplicacao (double  num1, double num2){
+public double Multiplicacao (double  num1, double num2){
     return num1 * num2;
     
 }  
 
 public static void main (String []args){
+    double num1 = 10;
+    double num2 = 20;
     Calculadora c = new Calculadora();
+    System.out.println(c.soma(num1,num2));
+    System.out.println(c.subtracao(num1,num2));
+    System.out.println(c.divisao(num1,num2));
+    System.out.println(c.Multiplicacao(num1,num2));
 }
 }
