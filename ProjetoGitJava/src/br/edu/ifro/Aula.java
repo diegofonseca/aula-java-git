4c6863bfcd01cdcabdf338c87c3000d03123da67
04647218228 <04647218228@JIPA-LABIN03-03.ifro.local>
2018-03-07 15:13:16 -0400

    Criando a classe Calculadora

--- ProjetoGitJava/src/br/edu/ifro/Aula.java
+++ ProjetoGitJava/src/br/edu/ifro/Aula.java
@@ -15,8 +15,9 @@ public class Aula {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println ("hello world");
+       
+        
+        
     }
     
 }
--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -6,6 +6,7 @@ package br.edu.ifro;
  * @author 04647218228
  */
 public class Calculadora {
+    
     public double soma (double j, double k){
         return j + k ;
     }
@@ -19,6 +20,13 @@ public class Calculadora {
         return j / k ;
     }
     public static void main (String [] args){
+        double num1 = 10;
+        double num2 = 20; 
         Calculadora cal = new Calculadora();
+        System.out.println (cal.soma(num1, num2));
+        System.out.println (cal.subitracao(num1, num2));
+        System.out.println (cal.multiplicacao(num1, num2));
+        System.out.println (cal.divisao(num1, num2));
+        
     }
 }
