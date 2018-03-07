676922c3805e7dc8c97c39d77ff82ae0ca45b620
03575834261 <03575834261@JIPA-LABIN03-07.ifro.local>
2018-03-07 15:09:08 -0400

    Criando a classe Calculadora

--- ProjetoGitJava/src/br/edu/ifro/Calculadora.java
+++ ProjetoGitJava/src/br/edu/ifro/Calculadora.java
@@ -25,5 +25,21 @@ public class Calculadora {
            return num1 * num2;
     }
           
-         
+           public static void main(String [] args){
+               Calculadora c = new Calculadora ();
+            double num1 =  Double.parseDouble(javax.swing.JOptionPane.showInputDialog("Digite um número:")),
+                   num2 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog("Digite outro número:"));         
+                    
+                c.soma(num1,num2);
+                c.subtracao(num1,num2);
+                c.divisao(num1,num2);
+                c.multiplicacao(num1,num2);
+                
+                System.out.println (c.soma(num1,num2));
+       
+                    System.out.println (c.subtracao(num1,num2));
+       
+                        System.out.println (c.divisao(num1,num2));
+                            System.out.println (c.multiplicacao(num1,num2));
+                    }
 }
